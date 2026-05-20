fbd2acd4a8ccda309cbf74c6238e467b4c307f91
khemiriahmed <khemiriahmed6@gmail.com>
2026-05-20 13:55:52 +0100

    auth

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from "../features/auth/authSlice";
 import articleReducer from "../features/articles/articleSlice";
 // import categoryReducer from "../features/categories/categorySlice";
 import commentReducer from "../features/comments/commentSlice";
+
 
 export const store = configureStore({
   reducer: {
     articles: articleReducer,
     // categories: categoryReducer,
     comments: commentReducer,
+    auth: authReducer,
   },
 });
