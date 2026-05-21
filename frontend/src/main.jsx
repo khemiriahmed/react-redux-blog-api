7e23036bb1cf09cab2d8442763edd1a981b1aec0
khemiriahmed <khemiriahmed6@gmail.com>
2026-05-21 15:23:23 +0100

    config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,11 @@ import "./index.css";
 import { store } from "./app/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+
 );
