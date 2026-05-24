339e1258ee4a6524588ed20210ac45de222a242b
khemiriahmed <khemiriahmed6@gmail.com>
2026-05-24 20:56:01 +0100

    add className

--- frontend/src/pages/ListeArticle.jsx
+++ frontend/src/pages/ListeArticle.jsx
@@ -120,7 +120,7 @@ function Home() {
               placeholder="Search articles..."
               value={search}
               onChange={(e) => setSearch(e.target.value)}
-              className="border px-4 py-2 rounded-xl w-64 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="border border-gray-300 rounded-xl px-4 py-2 w-80 focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
 
