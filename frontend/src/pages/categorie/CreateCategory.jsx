2515417ad8b756101a43772fc1d11cddc31d1d66
khemiriahmed <khemiriahmed6@gmail.com>
2026-05-30 16:54:10 +0100

    CreateCategory

--- frontend/src/pages/categorie/CreateCategory.jsx
+++ frontend/src/pages/categorie/CreateCategory.jsx
@@ -16,9 +16,7 @@ function CreateCategory() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const result = await dispatch(
-      createCategory({ name })
-    );
+    const result = await dispatch(createCategory({ name }));
 
     if (createCategory.fulfilled.match(result)) {
       navigate("/categories");
@@ -27,21 +25,13 @@ function CreateCategory() {
 
   return (
     <div className="max-w-xl mx-auto py-10">
-
-      <form
-        onSubmit={handleSubmit}
-        className="bg-white p-6 rounded-xl shadow"
-      >
-        <h1 className="text-2xl font-bold mb-5">
-          Create Category
-        </h1>
+      <form onSubmit={handleSubmit} className="bg-white p-6 rounded-xl shadow">
+        <h1 className="text-2xl font-bold mb-5">Create Category</h1>
 
         <input
           type="text"
           value={name}
-          onChange={(e) =>
-            setName(e.target.value)
-          }
+          onChange={(e) => setName(e.target.value)}
           placeholder="Category name"
           className="w-full border p-3 rounded-xl"
         />
