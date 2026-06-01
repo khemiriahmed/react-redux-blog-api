c02c071f40fea92f1a8d625d6b171842fb763cbb
khemiriahmed <khemiriahmed6@gmail.com>
2026-06-01 19:15:54 +0100

    App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import Navbar from "./components/Navbar";
-
+import Sidebar from "./components/Sidebar";
 import AppRoutes from "./routes/AppRoutes";
 
 import { fetchUser } from "./features/auth/authSlice";
@@ -25,11 +25,17 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <Navbar />
+    <div className="flex">
+      <Sidebar />
 
-      <AppRoutes />
-    </>
+      <div className="flex-1 ml-72">
+        <Navbar />
+
+        <div className="p-6">
+          <AppRoutes />
+        </div>
+      </div>
+    </div>
   );
 }
 
